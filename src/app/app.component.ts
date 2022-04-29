e9ef6f2174db2dfafc71f31fec7d4f8f05c06b12
oattsp <thodsaphon.aiab@gmail.com>
2022-04-29 20:45:20 +0700

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Status} from "./enum/status";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assessment-day2';
+  totalLike = 100;
+  totalDislike = 25;
+  userLikeStatus = Status.Normal;
+
+  userClickedButton(event: Status) {
+    if (event === Status.Like) {
+      if (this.userLikeStatus === Status.Normal) {
+        this.userLikeStatus = Status.Like;
+        this.totalLike++;
+        return
+      }
+      if (this.userLikeStatus === Status.Like) {
+        this.userLikeStatus = Status.Normal;
+        this.totalLike--;
+        return;
+      }
+
+      if (this.userLikeStatus === Status.Dislike) {
+        this.userLikeStatus = Status.Like;
+        this.totalDislike--;
+        this.totalLike++;
+        return;
+      }
+    }
+    if (event === Status.Dislike) {
+      if (this.userLikeStatus === Status.Normal) {
+        this.userLikeStatus = Status.Dislike;
+        this.totalDislike++;
+        return;
+      }
+      if (this.userLikeStatus === Status.Dislike) {
+        this.userLikeStatus = Status.Normal;
+        this.totalDislike--;
+        return;
+      }
+      if (this.userLikeStatus === Status.Like) {
+        this.userLikeStatus = Status.Dislike;
+        this.totalDislike++;
+        this.totalLike--;
+        return;
+      }
+    }
+  }
+
+  userLike() {
+    this.userClickedButton(Status.Like);
+  }
+
+  userUnlike() {
+    this.userClickedButton(Status.Dislike);
+  }
 }
